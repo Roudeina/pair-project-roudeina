19855938bacfd2801edbc89a3b8e9152008b0a54
Roudeina <roudeina.bentrad@gmail.com>
2021-04-06 16:57:22 +0100

    refactor

--- pairProject.js
+++ pairProject.js
@@ -1,15 +1,12 @@
-$( document ).ready(function() {
-    
-         $("[id^=s]").hide()
+$(document).ready(function () {
 
-
-        $("[id^=t]").click(function(){
-            $("[id^=s]").slideDown("slow")
-        });
-        
-        $("[id^=t]").click(function(){
-            $("[id^=s]").slideUp("slow")
-        });
-
-
+    $("[id^=s]").hide()
+    for (let i = 0; i < 20; i++) {
+        $(`#t${i}`).click(function () {
+            $(`#s${i}`).first().show("slow");
+            $(`#t${i}`).click(function () {
+                $(`#s${i}`).slideUp("slow")
+            })
+        })
+    }
 });
